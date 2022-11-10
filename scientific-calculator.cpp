be77a776912967ed841c74579ba1fd9d55ee5ed7
Mlemlus <mamlasmine@gmail.com>
2022-11-10 22:17:11 +0100

    Hotová definice tokenu

--- scientific-calculator.cpp
+++ scientific-calculator.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 char solver(char u_input[128]);
-
+Token ConvertToToken(char input_c);
 
 
 int main()
@@ -27,11 +27,11 @@ int main()
 
 char solver(char u_input[128]) {	// Přepíše vstup pomocí shunting yard algoritmu    https://en.wikipedia.org/wiki/Shunting_yard_algorithm
 	char Texas_Holdem;
-
 	char *p_u_input = u_input;
 	for (int i = 0; i < strlen(u_input); i++)
 	{
 		// Sjednocení char v jedno číslo
+		// dodělat poznání komplexu
 		if (isdigit(*p_u_input)) {	
 			Texas_Holdem = Texas_Holdem + *p_u_input;
 		}
@@ -41,7 +41,7 @@ char solver(char u_input[128]) {	// Přepíše vstup pomocí shunting yard algor
 			{
 				ConvertToToken(*p_u_input);
 			}
-			else {								
+			else {
 				ConvertToToken(Texas_Holdem);
 				Texas_Holdem = NULL;	// Konec sjednocení char v číslo
 			}
@@ -52,21 +52,31 @@ char solver(char u_input[128]) {	// Přepíše vstup pomocí shunting yard algor
 	return c_output;
 }
 
-void ConvertToToken(char input_c) {	// Z charu udělá Token
+Token ConvertToToken(char input_c) {	// Z charu udělá Token
 	if (isdigit(input_c)) {
-		printf("Numero %c \n", input_c);
-		//token_Output[0].value = input_c;
-		
+		printf("Numero %c \n", input_c); // vibe check
+		Token t = {0,input_c,0,NULL};
+		return t;
 	}
-	else if (isalpha)
-		printf("SUSSY ALPHA");
-	printf("%c \n", input_c);
-
-
-
-
-
-
-	// 1=
+	else if (isalpha) {
+		printf("CHAD %c \n", input_c); //vibe check
+		if (input_c == '^')
+			return Token{ 1,input_c,4,1 };
+		if (input_c == '*')
+			return Token{ 1,input_c,3,0 };
+		if (input_c == '/')
+			return Token{ 1,input_c,3,0 };
+		if (input_c == '+')
+			return Token{ 1,input_c,2,0 };
+		if (input_c == '-')
+			return Token{ 1,input_c,2,0 };
+		if (input_c == '(')
+			return Token{ 2,input_c,1,1 };
+		if (input_c == ')')
+			return Token{ 3,input_c,1,0 };
+	}
+	else {
+		printf("Něco nefachá: %c \n", input_c); //vibe check
+	}
 }
 
--- variables.cpp
+++ variables.cpp
@@ -1,25 +1,10 @@
 #include "variables.h"
 
 
-struct t_token
+typedef struct t_token
 {
-	int type; // 0 cislo, 1 operator, 2 (, 3 ), 4 ?funkce?, 5 other 
+	int type; // 0 cislo, 1 operator, 2 (, 3 ), 4 other 
 	const char value;
 	const int precedence;
 	const bool associativity; //0 left, 1 right
-}token_Output[64], token_Operator_Stack[64];
-	
-	
-/*
-class Token {
-public:
-	//enum type { Number, Operator, Function, Other };
-	int type;
-	const char value;
-	const int precedence;
-	const bool associativity; //0 left, 1 right
-
-	//Token(enum type, char value, int precedence, bool associativity);
-	Token(int type, char value, int precedence, bool associativity);
-};
-*/
+}token_Output[64], token_Operator_Stack[64], Token;
