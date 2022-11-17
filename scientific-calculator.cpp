2b68553f31ee5c1fdcc4c7a60882d6a8916a3b7a
Mlemlus <mamlasmine@gmail.com>
2022-11-17 03:01:46 +0100

    Solver hotov, první 3 řádky kódu fungují.

--- scientific-calculator.cpp
+++ scientific-calculator.cpp
@@ -21,12 +21,16 @@ typedef struct t_token
 char solver(char u_input[128]);
 Token ConvertToToken(char input_c);
 void AssignStack(Token token);
+void StacksPrint();
+
 
 //stacky
 Token token_Operator_Stack[64]; //stack
 Token token_Output[64];			//que
+double postFixStack[64];				//stack
 int OSlast = 0;
 int Olast = 0;
+int PFSlast = 0;
 
 
 int main()
@@ -37,20 +41,21 @@ int main()
 	scanf("%126s", &u_input);
 	//printf("%s\n", u_input);
 	solver(u_input);
+
 	getchar();
 	return 0;
 }
 
 char solver(char u_input[128]) {	// Přepíše vstup pomocí shunting yard algoritmu    https://en.wikipedia.org/wiki/Shunting_yard_algorithm
-	char digitComplet;
+	char digitComplet = NULL;
 	Token token = {};
 	char *p_u_input = u_input;
 	for (int i = 0; i < strlen(u_input); i++)
 	{
 		// Sjednocení char v jedno číslo
 		// dodělat poznání komplexu
-		if (isdigit(*p_u_input)) {	
-			digitComplet = digitComplet + *p_u_input;
+		if (isdigit(*p_u_input)) {
+			digitComplet = digitComplet + p_u_input[0];
 			p_u_input++;
 			continue;
 		}
@@ -74,9 +79,11 @@ char solver(char u_input[128]) {	// Přepíše vstup pomocí shunting yard algor
 		OSlast--;
 	}
 
+	StacksPrint(); //vibe check
 
-	char c_output = 'jep'; // placeholder aka vibe check
-	return c_output;
+	PostFixEvaluator();
+
+	return (char)(postFixStack[0]);
 }
 
 Token ConvertToToken(char input_c) {	// Z charu udělá Token
@@ -104,6 +111,7 @@ Token ConvertToToken(char input_c) {	// Z charu udělá Token
 	}
 	else {
 		printf("Něco nefachá: %c \n", input_c); //vibe check
+		return Token{ 4, input_c,0,0 };
 	}
 }
 
@@ -146,6 +154,36 @@ void AssignStack(Token token) { // dá token do stacku
 	}
 }
 
-void PostFixEvaluator() { // oh yeah, it's all coming together
+void StacksPrint() {
+	printf("Operator Stack: ");
+	for (int i = 0; i <= sizeof(token_Operator_Stack); i++)
+	{
+		printf("%c", token_Operator_Stack[i]);
+	}
+	printf("\nToken Output:");	
+	for (int i = 0; i <= sizeof(token_Output); i++)
+	{
+		printf("%c", token_Output[i]);
+	}
+	printf("\n");
+}
 
+void PostFixEvaluator() { // oh yeah, it's all coming together
+	for (int i = 0; i <= sizeof(token_Output); i++) {
+		if (token_Output[i].type == 0) {
+			postFixStack[PFSlast] = (double)(token_Output[i].value);
+			PFSlast++;
+		}
+		if (token_Output[i].type == 1) {
+			if (token_Output[i].value = '+')
+				postFixStack[PFSlast - 1] = postFixStack[OSlast - 1] + postFixStack[OSlast];
+			if (token_Output[i].value = '-')
+				postFixStack[PFSlast - 1] = postFixStack[OSlast - 1] - postFixStack[OSlast];
+			if (token_Output[i].value = '*')
+				postFixStack[PFSlast - 1] = postFixStack[OSlast - 1] * postFixStack[OSlast];
+			if (token_Output[i].value = '/')
+				postFixStack[PFSlast - 1] = postFixStack[OSlast - 1] / postFixStack[OSlast];	
+			PFSlast--;
+		}
+	}
 }
