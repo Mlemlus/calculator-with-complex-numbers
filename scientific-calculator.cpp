c99ff552fa1da89d75b22d70a3d0ab9b6ffbefa8
Mlemlus <mamlasmine@gmail.com>
2022-12-05 22:51:03 +0100

    Přidání: ukládání výsledků, výpočet ze souborů, příkazy, clear stacků a fronty, pořádný výpis výsledku. Smazání neužitečných komentářů.
    Signed-off-by: Mlemlus <mamlasmine@gmail.com>

--- scientific-calculator.cpp
+++ scientific-calculator.cpp
@@ -1,18 +1,9 @@
-﻿// scientific-calculator.cpp: Definuje vstupní bod pro aplikaci.
-//
-
-#include "scientific-calculator.h"
-//#include "variables.h"
-#include <stdarg.h>
-//#include <complex.h>
-
-
+﻿#include "scientific-calculator.h"
 using namespace std;
-typedef struct complex {
+struct complex {
 	double real;
 	double imaginary;
 };
-
 typedef struct t_token
 {
 	int type; // 0 cislo, 1 operator, 2 (, 3 ), 4 other 
@@ -21,76 +12,122 @@ typedef struct t_token
 	int precedence;
 	bool associativity; //0 left, 1 right
 }Token;
+struct t_result {
+	int id;
+	char result[256];
+	struct t_result* next;
+};
+struct t_result* firstResult = NULL;
 
-//funkce ci metody nebo jak se tomu nadava 
-char solver(char u_input[128]);
+//funkce 
+void solver(char u_input[128]);
 Token ConvertToToken(char input_c);
 void AssignStack(Token token);
-void StacksPrint();
 void PostFixEvaluator();
-
+void PrintResult(double r, double i,char *result);
+void SaveResult(char *result,char path[]);
+void LoadResults(bool silent,char path[]);
+Token ResultPicker(int id);
+void Commands(char input);
+void FileSolver(char* inputPath, char* outputPath);
+void ClearStacks();
 
 
 //stacky
-Token token_Operator_Stack[64]; //stack
-Token token_Output[64];			//que
-complex postFixStack[64];				//stack
-int OSlast = 0;		//posledni free
-int Olast = 0;		//posledni free
-int PFSlast = - 1;	//POSLEDNÍ S HODNOTOU
+Token token_Operator_Stack[256]; //stack
+Token token_Output[256];		 //que
+complex postFixStack[256];	 	 //stack
+int OSlast;		//posledni free
+int Olast;		//posledni free
+int PFSlast;	//POSLEDNÍ S HODNOTOU
+bool repeat = "az naprsi a uschne";
+char savedPath[] = "saved";
+
 
 
 int main()
 {
-	// desetinny cisla add////////////////////////
-	char u_input[128];
-	for (int i = 0; i < 128; i++)
-	{u_input[i] = '\0';}
-	printf("Priklad zapisu:\n(12-(5+5i)-2^(1+1))/(0-2i^(2))\n"); // Dle casio classwiz = 1.5 - 2.5i
-	printf("max 126 znaku\n");
-	scanf("%126s", &u_input);
-	//printf("%s\n", u_input);
-	solver(u_input);
-
-	getchar();
+	printf("?h pro manual\n");
+	while (repeat == true)
+	{
+		ClearStacks();
+		LoadResults(true,savedPath);
+		char u_input[128]; char result[256];
+		printf("max 126 znaku\n");
+		scanf("%126s", u_input);
+		solver(u_input);
+		PrintResult(postFixStack[0].real, postFixStack[0].imaginary, result);
+		if (result[0] != '0') {
+			char buffer=NULL;
+			printf("%s", result);
+			printf("\nUlozit? y/n \n");
+			scanf(" %c", &buffer);
+			if (buffer == 'y')
+			{
+				SaveResult(result, savedPath);
+				printf("ulozeno\n");
+			}
+		}
+	}
+	printf("Uschlo");
 	return 0;
 }
+void ClearStacks() {
+	OSlast = 0; Olast = 0; PFSlast = -1;
+	memset(postFixStack, NULL, sizeof(postFixStack));
+	memset(token_Operator_Stack, NULL, sizeof(token_Operator_Stack));
+	memset(token_Output, NULL, sizeof(token_Output));
+}
 
-char solver(char u_input[128]) {	// Přepíše vstup pomocí shunting yard algoritmu    https://en.wikipedia.org/wiki/Shunting_yard_algorithm
-	char numUnify[128];
+void solver(char u_input[128]) {	// Přepíše vstup pomocí shunting yard algoritmu    https://en.wikipedia.org/wiki/Shunting_yard_algorithm
+	char numUnify[1024];
 	int numCount = 0;
 	char *ptr = u_input;
+	if (*ptr == '?') {
+		ptr++;
+		Commands(*ptr);
+		ClearStacks();
+		return;
+	}
 	while(*ptr != '\0' || numCount != 0) {
 		// Sjednocení char v jedno číslo
 		if (*ptr != '\0') {
-			if (isdigit(*ptr)) {
-				numUnify[numCount] = *ptr;
+			if (*ptr == 'A') { // nacte vysledek dle kodu(ID)
+				ptr++; char buffer[128]; int ibuff = 0;
+				while (isdigit(*ptr)) {
+					buffer[ibuff] = *ptr;
+					ibuff++; ptr++;
+				}
+				buffer[ibuff] = '\0';
+				AssignStack(ResultPicker(atoi(buffer)));
+			}
+			if (isdigit(*ptr) || *ptr == ',' || *ptr == '.') {
+				*ptr == ',' ? numUnify[numCount] = '.' : numUnify[numCount] = *ptr;
 				ptr++; numCount++;
 				continue;
 			}
 		}
-		if (numCount == 0) {
+		if (numCount == 0 && *ptr != '\0') { // pro operandy
 			AssignStack(ConvertToToken(*ptr));
 		}
-		else {
+		else { //pro cisla
 			numUnify[numCount] = '\0';
 			Token tukTuk{ 0,'n',complex{0,0},0,NULL };
 			if (*ptr == 'i') {
+				numCount == 0 ? tukTuk.value.imaginary = 1: tukTuk.value.imaginary = atof(numUnify);
 				tukTuk.value.imaginary = atof(numUnify);
 				AssignStack(tukTuk);
-				printf("NUMR %c \n", tukTuk.content); //vibe check
-
 			}else{
 				tukTuk.value.real = atof(numUnify);
 				AssignStack(tukTuk);				// assign num do outputu
-				printf("NUMR %c \n", tukTuk.content); //vibe check
 				if (*ptr != '\0') {
 					AssignStack(ConvertToToken(*ptr));  // assign aktualniho (neco jiného než num)
 				}
 			}
 			numCount = 0;
 		}
-		ptr++;
+		if(*ptr != '\0')
+			ptr++;
 	}
 
 
@@ -100,17 +137,12 @@ char solver(char u_input[128]) {	// Přepíše vstup pomocí shunting yard algor
 		Olast++;
 		OSlast--;
 	}
-
-	StacksPrint(); //vibe check
-
 	PostFixEvaluator();
-
-	return 0;
+	return;
 }
 
 Token ConvertToToken(char input_c) {	// Z charu udělá Token
 	if (isalpha) {
-		printf("CHAD %c \n", input_c); //vibe check
 		if (input_c == '^')
 			return Token{ 1,input_c,complex{},4,1};
 		if (input_c == '*')
@@ -125,9 +157,11 @@ Token ConvertToToken(char input_c) {	// Z charu udělá Token
 			return Token{ 2,input_c,complex{},0,1};
 		if (input_c == ')')
 			return Token{ 3,input_c,complex{},0,0};
+		if (input_c == 'i')
+			return Token{ 0,'i',complex{0,1},0,NULL};
 	}
 	else {
-		printf("Něco nefachá: %c \n", input_c); //vibe check
+		printf("Něco nefachá: %c \n", input_c);
 		return Token{ 4, input_c,0,0 };
 	}
 }
@@ -136,7 +170,7 @@ void AssignStack(Token token) { // dá token do stacku
 	if (token.type == 0) { // číslo
 		token_Output[Olast] = token;
 		Olast++;
-	}
+	}else
 
 	if (token.type == 1) { // operátor
 		// check precedence předchozího operátoru ve OperatorStacku
@@ -151,12 +185,12 @@ void AssignStack(Token token) { // dá token do stacku
 		else {
 			printf("Precedance err %c", token.content);
 		}
-	}
+	}else
 
 	if (token.type == 2) { // (
 		token_Operator_Stack[OSlast] = token;
 		OSlast++;
-	}
+	}else
 
 	if (token.type == 3) { // )
 		for (int y = OSlast - 1; y >= 0; y--)
@@ -176,36 +210,7 @@ void AssignStack(Token token) { // dá token do stacku
 	}
 }
 
-void StacksPrint() {
-	/*
-	printf("Operator Stack: ");
-	for (int i = 0; i < 64; i++)
-	{
-		if (token_Operator_Stack[i].content != '\0') {
-			if (token_Operator_Stack[i].type == 0)
-			{
-				printf("(%lf + %lf i)", token_Operator_Stack[i].value.real, token_Operator_Stack[i].value.imaginary);
-			}else
-				printf("%c", token_Operator_Stack[i].content);
-		}
-	}
-	printf("\nToken Output:");	
-	for (int i = 0; i < 64; i++)
-	{
-		if (token_Operator_Stack[i].content != '\0') {
-			if (token_Output[i].type == 0)
-			{
-				printf("(%lf + %lf i)", token_Output[i].value.real, token_Output[i].value.imaginary);
-			}
-			else
-				printf("%c", token_Output[i].content);
-		}
-	}
-	printf("\n");
-	*/
-}
-
-void PostFixEvaluator() { // oh yeah, it's all coming together
+void PostFixEvaluator() { 
 	for (int i = 0; i < 64; i++) {
 		if(token_Output[i].content != '\0')
 		{
@@ -217,22 +222,22 @@ void PostFixEvaluator() { // oh yeah, it's all coming together
 				if (token_Output[i].content == '+') {
 					postFixStack[PFSlast - 1].real = postFixStack[PFSlast - 1].real + postFixStack[PFSlast].real;
 					postFixStack[PFSlast - 1].imaginary = postFixStack[PFSlast - 1].imaginary + postFixStack[PFSlast].imaginary;
-				}
+				} else
 				if (token_Output[i].content == '-') {
 					postFixStack[PFSlast - 1].real = postFixStack[PFSlast - 1].real - postFixStack[PFSlast].real;
 					postFixStack[PFSlast - 1].imaginary = postFixStack[PFSlast - 1].imaginary - postFixStack[PFSlast].imaginary;
-				}
+				} else
 				if (token_Output[i].content == '*') {
 					postFixStack[PFSlast - 1].real = postFixStack[PFSlast - 1].real * postFixStack[PFSlast].real;
 					postFixStack[PFSlast - 1].imaginary = postFixStack[PFSlast - 1].imaginary * postFixStack[PFSlast].imaginary;
-				}
+				} else
 				if (token_Output[i].content == '/') {
 					if (postFixStack[PFSlast].real != 0) {
 						postFixStack[PFSlast - 1].real = postFixStack[PFSlast - 1].real / postFixStack[PFSlast].real;
 						postFixStack[PFSlast - 1].imaginary = postFixStack[PFSlast - 1].imaginary / postFixStack[PFSlast].real;
 					}
-				}
-				if (token_Output[i].content == '^') { //THE SAUCE
+				} else
+				if (token_Output[i].content == '^') {
 					if(postFixStack[PFSlast - 1].real != 0 && postFixStack[PFSlast].real != 0) // nelze 0^0 (v komplex)
 						postFixStack[PFSlast - 1].real = pow(postFixStack[PFSlast - 1].real, postFixStack[PFSlast].real);
 
@@ -260,12 +265,217 @@ void PostFixEvaluator() { // oh yeah, it's all coming together
 								break;
 						}
 					}else{
-						printf("Imaginarni cisla lze mocnit pouze celymi cisly: %4lf ^ %4lf", postFixStack[PFSlast - 1].imaginary, num2);
+						printf("Imaginarni cisla lze mocnit pouze celymi cisly: %4lf ^ %4lf", postFixStack[PFSlast - 1].imaginary, postFixStack[PFSlast].real);
 					}
 				}
 				PFSlast--;
 			}
 		}
 	}
-	printf("(%4lf + %4lf i)", postFixStack[0].real, postFixStack[0].imaginary);
+}
+
+void PrintResult(double r, double i,char *result) {
+	while (false == false) {
+		if ((int)(round(r)) == 0) {
+			snprintf(result,sizeof(result),"%.4lfi\0", i);
+			break;
+		}
+		if ((int)(round(i)) == 0) {
+			snprintf(result, sizeof(result), "%.4lf\0", r);
+			break;
+		}
+		i < 0 ? snprintf(result, 256, "%.4lf%.4lfi\0", r, i) : snprintf(result, 256, "%.4lf+%.4lfi\0", r, i);
+		break;
+	}
+}
+
+void SaveResult(char *result, char path[]) {
+	FILE* f;
+	f = fopen(path, "a+");
+	if (f == NULL) {
+		printf("Prazny soubor\n");
+		return;
+	}
+	fprintf(f, "%s", result);
+	fprintf(f, ";");
+	fclose(f);
+}
+
+void LoadResults(bool silent,char path[]) {
+	free(firstResult);
+	firstResult = NULL;
+	struct t_result* newResult;
+	struct t_result* aktResult;
+	newResult = (struct t_result*)malloc(sizeof(struct t_result));
+	aktResult = (struct t_result*)malloc(sizeof(struct t_result));
+
+
+	FILE *f = fopen(path, "r");
+	if (f == NULL) {
+		if(silent==false)
+			printf("Prazny soubor\n");
+		return;
+	}
+	int id = 0 ,ibuff = 0;
+	char buffer[256];
+	int c = fgetc(f);
+	while (c != EOF) {
+		if ((char)(c) == ';') {
+			newResult = (struct t_result*)malloc(sizeof(struct t_result));
+			buffer[ibuff] = '\0';
+			newResult->id = id;
+			newResult->next = NULL;
+			strcpy_s(newResult->result, 256, buffer);
+			if (firstResult == NULL) {
+				firstResult = newResult;
+			}
+			else {
+				aktResult->next = newResult;
+			}
+			aktResult = newResult;
+			buffer[0] = '\0';
+			ibuff = 0; id++;
+			c = fgetc(f);
+			continue;
+		}
+		buffer[ibuff] = (char)(c);
+		ibuff++;
+		c = fgetc(f);
+	}
+	fclose(f);
+	if(silent == false){
+		aktResult = firstResult;
+		printf("\nKod | vysledek\n");
+		while (aktResult)
+		{
+			printf("A%d | %s\n", aktResult->id,aktResult->result);
+			if (aktResult->next == NULL)
+				break;
+			aktResult = aktResult->next;
+		}
+		printf("\n");
+	}
+}
+
+Token ResultPicker(int id) {
+	Token token{ 0,'n',complex{0,0},0,NULL };
+	struct t_result* aktResult;
+	if (firstResult == NULL) {
+		LoadResults(true,savedPath);
+	}if (firstResult == NULL) {
+		printf("Zadne ulozene vysledky");
+		return token;
+	}
+	aktResult = firstResult;
+	while (aktResult)
+	{
+		if (id == aktResult->id) {
+			char buffer[256]; int ibuff = 0;
+			char* Rptr = aktResult->result;
+			while (*Rptr != '\0') {
+				if (isdigit(*Rptr) || *Rptr == ',' || *Rptr == '.') {
+					*Rptr == ',' ? buffer[ibuff] = '.' : buffer[ibuff] = *Rptr;
+					Rptr++; ibuff++;
+					continue;
+				}
+				else {
+					buffer[ibuff] = '\0';
+					if (*Rptr == 'i') {
+						token.value.imaginary = atof(buffer);
+						ibuff = 0;
+						buffer[0] = '\0';
+					}
+					else if (*Rptr == '+') {
+						token.value.real = atof(buffer);
+						ibuff = 0;
+						buffer[0] = '\0';
+					}
+					else if (*Rptr == '-') {
+						if (ibuff == 0) {
+							buffer[ibuff] = '-';
+							ibuff++;
+						}
+						else {
+							token.value.real = atof(buffer);
+							ibuff = 1;
+							buffer[0] = '-';
+						}
+					}
+					Rptr++;
+				}
+			}
+			if (ibuff != 0) {
+				token.value.real = atof(buffer);
+			}
+			return token;
+		}
+		aktResult = aktResult->next;
+	}
+	printf("Vysledek A%d nenalezen", id);
+	return token;
+}
+
+void Commands(char input) {
+	switch (input) {
+		case 'h':
+			printf("\nPredchozi vysledky se pisou v kodu primu do vypoctu, viz. ?e\n");
+			printf("Prikazy se zadavaji otaznikem + prikazem napr. ?h\n");
+			printf("?h - Vypsat manual\n");
+			printf("?l - (L)Vypsat ulozene\n");
+			printf("?e - Vypsat priklady zapisu \n");
+			printf("?c - Vycisti ulozene vysledky \n");
+			printf("?x - Vypocet ze souboru \n");
+			printf("?q - ukonceni programu\n\n");
+			break;
+		case 'e':
+			printf("Priklad zapisu:\n(12-(5+5i)-2^(1+1))/(0-2i^(2)) \n");// Dle casio classwiz = 1.5 - 2.5i
+			printf("10i^25-300/5-i | ");								// Dle casio classwiz = -60 + 9i
+			printf("-i+2i | ");										// Dle casio classwiz = 1i
+			printf("i-2i+A2-A3 | ");
+			printf("1-2-1i+1i | ");
+			printf("2-1-1i+1i\n");
+			break;
+		case 'l':
+			LoadResults(false,savedPath);
+			break;
+		case 'q':
+			repeat = false;
+			break;
+		case 'x':
+			char inputPath[256]; char outputPath[256];
+			printf("Zadejte cesty k souborum ve tvaru: souborKVypoctu.txt souborKZapsaniVysledku.txt\n");
+			scanf("%256s %256s", &inputPath, &outputPath);
+			FileSolver(inputPath, outputPath);
+			break;
+		case 'c':
+			fclose(fopen(savedPath,"w"));
+			break;
+	}
+}
+
+void FileSolver(char *inputPath, char *outputPath) {
+	FILE* f = fopen(inputPath, "r");
+	if (f == NULL) {
+		printf("Prazny soubor\n");
+		return;
+	}
+	int ibuff = 0; char buffer[256];
+	int c = fgetc(f);
+	while (c != EOF) {
+		if ((char)(c) == ';') {
+			ClearStacks();
+			char result[256];
+			buffer[ibuff] = '\0';
+			solver(buffer);
+			PrintResult(postFixStack[0].real, postFixStack[0].imaginary, result);
+			SaveResult(result,outputPath);
+			c = fgetc(f);
+			ibuff = 0;
+			continue;
+		}
+		buffer[ibuff] = (char)(c);
+		ibuff++;
+		c = fgetc(f);
+	}
+	fclose(f);
 }
